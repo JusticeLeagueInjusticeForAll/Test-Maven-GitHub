c1d3e45a618157f382c93f056d543d51a718ab2b
杨成铭 <@192.168.1.110>
2018-02-01 20:26:47 +0800

    ss

--- jeeweb Maven Webapp/src/main/java/cn/jbit/dao/BBB.java
+++ jeeweb Maven Webapp/src/main/java/cn/jbit/dao/BBB.java
@@ -5,7 +5,7 @@ public class BBB {
 	public void show(){
 		System.out.println("222");
 		System.out.println("444");
-		System.out.println("222");
+		System.out.println("2223");
 		System.out.println("444");
 	}
 	
